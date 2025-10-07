7a42e22f10e0024d36c8096eda8409232ab673d6
Matthieu Droz <matthieu.droz@gmail.com>
2025-10-07 22:07:35 +0200

    Clean plate to start

--- app/src/main/java/com/swentseekr/seekr/model/author/Author.kt
+++
@@ -1,9 +0,0 @@
-package com.swentseekr.seekr.model.author
-
-data class Author(
-    val pseudonym: String,
-    val bio: String,
-    val profilePicture: Int,
-    val reviewRate: Double,
-    val sportRate: Double
-)
