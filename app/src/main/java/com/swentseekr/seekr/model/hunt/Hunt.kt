77b433e2f9c460aed226c0e34b8f66920903198c
anazaric <ana.zaric@epfl.ch>
2025-10-09 11:10:31 +0200

    feat(hunt): implement OverviewViewModel and the Hunt repository
    Implement logic of the Overview: Screen,impelent the function
    Implement the logic of the HuntRepository: the local functions, list of hunts for visual testing

--- app/src/main/java/com/swentseekr/seekr/model/hunt/Hunt.kt
+++ app/src/main/java/com/swentseekr/seekr/model/hunt/Hunt.kt
@@ -1,9 +1,11 @@
-package com.swentseekr.seekr.model.hunt
+package com.android.sample.model.hunt
 
 import com.swentseekr.seekr.model.author.Author
 import com.swentseekr.seekr.model.map.Location
 
+
 data class Hunt(
+    val uid: String,
     val start: Location,
     val end: Location,
     val middlePoints: List<Location>,
@@ -15,16 +17,33 @@ data class Hunt(
     val difficulty: Difficulty,
     val author: Author,
     val image: Int,
+    val reviewRate: Double
 )
 
 enum class HuntStatus {
-  FUN,
-  DISCOVER,
-  SPORT
+    FUN,
+    DISCOVER,
+    SPORT
 }
 
 enum class Difficulty {
-  EASY,
-  INTERMEDIATE,
-  DIFFICULT
+    EASY,
+    INTERMEDIATE,
+    DIFFICULT
+}
+fun DifficultyColor(difficulty: Difficulty): Int {
+    return when (difficulty) {
+        Difficulty.EASY -> 0x60BA37 // Green
+        Difficulty.INTERMEDIATE ->   0xFFDFAD // Yellow
+        Difficulty.DIFFICULT ->   0xFFC1C1// Red
+    }
+
+}
+fun StatusColor(status: HuntStatus): Int {
+    return when (status) {
+        HuntStatus.FUN -> 0x60BA37 // Green
+        HuntStatus.DISCOVER -> 0xFFDFAD // Yellow
+        HuntStatus.SPORT -> 0xFFC1C1 // Red
+    }
+
 }
