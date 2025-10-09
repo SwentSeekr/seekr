653a44137b2c7759a05681f258a8b5c1b7b6ee3c
anazaric <ana.zaric@epfl.ch>
2025-10-09 11:18:22 +0200

    fix(hunt): ktfmtFormat
    Correct the format

--- app/src/main/java/com/swentseekr/seekr/model/hunt/Hunt.kt
+++ app/src/main/java/com/swentseekr/seekr/model/hunt/Hunt.kt
@@ -3,7 +3,6 @@ package com.android.sample.model.hunt
 import com.swentseekr.seekr.model.author.Author
 import com.swentseekr.seekr.model.map.Location
 
-
 data class Hunt(
     val uid: String,
     val start: Location,
@@ -21,29 +20,29 @@ data class Hunt(
 )
 
 enum class HuntStatus {
-    FUN,
-    DISCOVER,
-    SPORT
+  FUN,
+  DISCOVER,
+  SPORT
 }
 
 enum class Difficulty {
-    EASY,
-    INTERMEDIATE,
-    DIFFICULT
+  EASY,
+  INTERMEDIATE,
+  DIFFICULT
 }
+
 fun DifficultyColor(difficulty: Difficulty): Int {
-    return when (difficulty) {
-        Difficulty.EASY -> 0x60BA37 // Green
-        Difficulty.INTERMEDIATE ->   0xFFDFAD // Yellow
-        Difficulty.DIFFICULT ->   0xFFC1C1// Red
-    }
-
+  return when (difficulty) {
+    Difficulty.EASY -> 0x60BA37 // Green
+    Difficulty.INTERMEDIATE -> 0xFFDFAD // Yellow
+    Difficulty.DIFFICULT -> 0xFFC1C1 // Red
+  }
 }
-fun StatusColor(status: HuntStatus): Int {
-    return when (status) {
-        HuntStatus.FUN -> 0x60BA37 // Green
-        HuntStatus.DISCOVER -> 0xFFDFAD // Yellow
-        HuntStatus.SPORT -> 0xFFC1C1 // Red
-    }
 
+fun StatusColor(status: HuntStatus): Int {
+  return when (status) {
+    HuntStatus.FUN -> 0x60BA37 // Green
+    HuntStatus.DISCOVER -> 0xFFDFAD // Yellow
+    HuntStatus.SPORT -> 0xFFC1C1 // Red
+  }
 }
--- app/src/main/java/com/swentseekr/seekr/model/hunt/HuntRepositoryProvider.kt
+++ app/src/main/java/com/swentseekr/seekr/model/hunt/HuntRepositoryProvider.kt
@@ -6,33 +6,31 @@ import com.android.sample.model.hunt.HuntStatus
 import com.swentseekr.seekr.model.author.Author
 import com.swentseekr.seekr.model.map.Location
 
-
 /**
  * Provides a single instance of the repository in the app. `repository` is mutable for testing
  * purposes.
  */
 object HuntRepositoryProvider {
-    private val _repository: HuntsRepository =
-        HuntsRepositoryLocal().apply {
-            val sampleHunts =
-                listOf(
-                    Hunt(
-                        uid = getNewUid(),
-                        start = Location(40.7128, -74.0060, "New York"),
-                        end = Location(40.730610, -73.935242, "Brooklyn"),
-                        middlePoints = emptyList(),
-                        status = HuntStatus.FUN,
-                        title = "City Exploration",
-                        description = "Discover hidden gems in the city",
-                        time = 2.5,
-                        distance = 5.0,
-                        difficulty = Difficulty.EASY,
-                        author = Author("spike man", "", 1, 2.5, 3.0),
-                        image = 0,
-                        reviewRate = 4.5
-                    ),
-                )
+  private val _repository: HuntsRepository =
+      HuntsRepositoryLocal().apply {
+        val sampleHunts =
+            listOf(
+                Hunt(
+                    uid = getNewUid(),
+                    start = Location(40.7128, -74.0060, "New York"),
+                    end = Location(40.730610, -73.935242, "Brooklyn"),
+                    middlePoints = emptyList(),
+                    status = HuntStatus.FUN,
+                    title = "City Exploration",
+                    description = "Discover hidden gems in the city",
+                    time = 2.5,
+                    distance = 5.0,
+                    difficulty = Difficulty.EASY,
+                    author = Author("spike man", "", 1, 2.5, 3.0),
+                    image = 0,
+                    reviewRate = 4.5),
+            )
 
-        }
-    var repository: HuntsRepository = HuntRepositoryProvider._repository
+      }
+  var repository: HuntsRepository = HuntRepositoryProvider._repository
 }
