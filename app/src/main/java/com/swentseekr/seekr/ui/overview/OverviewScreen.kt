bd403ee3279ce2744970d8c4584f4380bb4bd921
kaazisma <netodiasisaac@gmail.com>
2025-10-10 10:54:36 +0200

    fix

--- app/src/main/java/com/swentseekr/seekr/ui/overview/OverviewScreen.kt
+++ app/src/main/java/com/swentseekr/seekr/ui/overview/OverviewScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.SearchBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
