ff16a5b905213034e25062f5a350110c13a161e1
Gauthier7411 <gauthier.dpx@gmail.com>
2025-10-11 16:36:18 +0200

    test(ui): add instrumented UI tests for SignInScreen
    Added a new Compose UI test class `SignInScreenTest` under `src/androidTest`
    to verify the visual components and basic interactions of the SignIn screen.

---
+++ app/src/androidTest/java/com/swentseekr/seekr/screen/SignInScreenTest.kt
@@ -0,0 +1,42 @@
+package com.swentseekr.seekr.screen
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.firebase.auth.FirebaseAuth
+import com.swentseekr.seekr.ui.auth.SignInScreen
+import com.swentseekr.seekr.ui.auth.SignInScreenTestTags.APP_LOGO
+import com.swentseekr.seekr.ui.auth.SignInScreenTestTags.LOGIN_BUTTON
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class SignInScreenTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Before
+    fun setUp() {
+        FirebaseAuth.getInstance().signOut()
+    }
+
+    @Test
+    fun signInScreen_componentsAreDisplayed() {
+        composeTestRule.setContent { SignInScreen() }
+
+        composeTestRule.onNodeWithTag(APP_LOGO).assertIsDisplayed()
+        composeTestRule.onNodeWithTag(LOGIN_BUTTON).assertIsDisplayed()
+    }
+
+    @Test
+    fun clickingLoginButton_triggersSignInAttempt() {
+        composeTestRule.setContent { SignInScreen() }
+
+        composeTestRule.onNodeWithTag(LOGIN_BUTTON).assertIsDisplayed().performClick()
+    }
+}
