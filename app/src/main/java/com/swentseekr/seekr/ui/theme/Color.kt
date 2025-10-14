9c62c91f0fb48d61fa4c058e383d86dd4589e70b
anazaric <ana.zaric@epfl.ch>
2025-10-14 15:27:54 +0200

    Change the Theme of the project in order to match the Figma update color and typography definitions for improved theme

--- app/src/main/java/com/swentseekr/seekr/ui/theme/Color.kt
+++ app/src/main/java/com/swentseekr/seekr/ui/theme/Color.kt
@@ -9,6 +9,29 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
-val GrassGreen = Color(0xFF4CAF50)
+//val GrassGreen = Color(0xFF4CAF50)
 val Black = Color(0xFF000000)
 val White = Color(0xFFFFFFFF)
+
+val Green = Color(0xFF60BA37)
+val Orange = Color(0xFFFFA64D)
+val GrayBackgound = Color(0xFFF2F2F2)
+
+val EasyGreen = Color(0xFFB0D4B1)
+val MediumYellow = Color(0xFFFFDFAD)
+val DifficultRed = Color(0xFFFFC1C1)
+
+val GrayDislike = Color(0xFF666666)
+val RedLike = Color(0xFFF90606)
+
+val YellowStar = Color(0xFFE7CD0C)
+val GrayStar = Color(0xFF838382)
+val RedRanking = Color(0xFFFF3C00)
+val GrayRanking = Color(0xFF838382)
+
+val LightError = Color(0xFFBA1A1A)
+val LightOnError = Color(0xFFFFFFFF)
+
+val DarkError = Color(0xFFFFB4AB)
+val DarkOnError = Color(0xFF690005)
+
