d4b04ba23921a2633bed150af7442d10c8bcd2e1
Gauthier7411 <gauthier.dpx@gmail.com>
2025-10-29 16:42:04 +0100

    Add a view model for settings screen
    Update builg.gradle to force gradle to generate buildConfig

---
+++ app/src/main/java/com/swentseekr/seekr/ui/settings/SettingsViewModel.kt
@@ -0,0 +1,52 @@
+package com.swentseekr.seekr.ui.settings
+
+import androidx.credentials.ClearCredentialStateRequest
+import androidx.credentials.CredentialManager
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.swentseekr.seekr.model.authentication.AuthRepository
+import com.swentseekr.seekr.model.authentication.AuthRepositoryFirebase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import com.swentseekr.seekr.BuildConfig
+
+data class SettingsUIState(
+    val signedOut: Boolean = false,
+    val errorMsg: String? = null,
+    val appVersion: String? = null
+)
+
+class SettingsViewModel(private val authRepository: AuthRepository = AuthRepositoryFirebase()) :
+    ViewModel() {
+
+  private val _uiState = MutableStateFlow(SettingsUIState())
+  val uiState: StateFlow<SettingsUIState> = _uiState.asStateFlow()
+
+      init {
+        displayAppVersion(BuildConfig.VERSION_NAME)
+      }
+
+  fun signOut(credentialManager: CredentialManager): Unit {
+    viewModelScope.launch {
+      authRepository
+          .signOut()
+          .fold(
+              onSuccess = { _uiState.update { it.copy(signedOut = true) } },
+              onFailure = { throwable ->
+                _uiState.update { it.copy(errorMsg = throwable.localizedMessage) }
+              })
+      credentialManager.clearCredentialState(ClearCredentialStateRequest())
+    }
+  }
+
+  fun clearErrorMsg() {
+    _uiState.update { it.copy(errorMsg = null) }
+  }
+
+  fun displayAppVersion(version: String) {
+    _uiState.update { it.copy(appVersion = version) }
+  }
+}
