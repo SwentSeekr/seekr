a500d1cdb1b39d4b6dacfd2213ce44098eeb1f42
anazaric <ana.zaric@epfl.ch>
2025-11-21 05:13:57 +0100

    fix: remove the commented code

--- app/src/main/java/com/swentseekr/seekr/model/hunt/ReviewImageRepository.kt
+++ app/src/main/java/com/swentseekr/seekr/model/hunt/ReviewImageRepository.kt
@@ -18,7 +18,6 @@ class ReviewImageRepository(private val storage: FirebaseStorage = FirebaseStora
 
   override suspend fun deleteReviewPhoto(url: String) {
     val ref = storage.fromDownloadUrl(url)
-    // val ref = storage.getReferenceFromUrl(url)
     ref.delete().await()
   }
 }
