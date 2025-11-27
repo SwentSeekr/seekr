0cc5d272dab88afa209f092f814381ef5fa5d03c
anazaric <ana.zaric@epfl.ch>
2025-11-27 20:37:05 +0100

    correct the code like the reviewer said to do

---
+++ app/src/androidTest/java/com/swentseekr/seekr/ui/review/AddReviewTestConstant.kt
@@ -0,0 +1,10 @@
+package com.swentseekr.seekr.ui.review
+
+object AddReviewTestConstantStings {
+  const val TestHuntId = "hunt123"
+  const val Comment = "Great hunt!"
+  const val TextInput = "Hello world"
+  const val Photo1 = "photo1"
+  const val Photo2 = "photo2"
+  const val TestTagLazyRow = "PhotosLazyRow"
+}
