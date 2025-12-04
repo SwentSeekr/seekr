9ced4c96d5446f6fcc95a30f45a8998267d96929
anazaric <ana.zaric@epfl.ch>
2025-12-04 11:21:35 +0100

    resolve sonar cloud issues

--- app/src/main/java/com/swentseekr/seekr/ui/hunt/review/AddReview.kt
+++ app/src/main/java/com/swentseekr/seekr/ui/hunt/review/AddReview.kt
@@ -42,51 +42,6 @@ fun AddReviewScreen(
       onCancel = onCancel,
       reviewViewModel = reviewViewModel,
       huntCardViewModel = huntCardViewModel)
-  /*
-  val uiState by reviewViewModel.uiState.collectAsState()
-  val uiStateHuntCard by huntCardViewModel.uiState.collectAsState()
-  LaunchedEffect(huntId) { reviewViewModel.loadHunt(huntId) }
-  val hunt2 = uiState.hunt
-  val authorId = hunt2?.authorId ?: ""
-  LaunchedEffect(authorId) { huntCardViewModel.loadAuthorProfile(authorId) }
-  val authorProfile = uiStateHuntCard.authorProfile
-  val hunt = uiState.hunt
-  var selectedImages by remember { mutableStateOf<List<Uri>>(emptyList()) }
-  val imagePickerLauncher =
-      rememberLauncherForActivityResult(
-          contract = ActivityResultContracts.GetMultipleContents(),
-          onResult = { uris ->
-            selectedImages = uris
-            uris.forEach { uri -> reviewViewModel.addPhoto(uri.toString()) }
-          })
-
-  val author = authorProfile?.author?.pseudonym ?: ("Unknown Author")
-  val context = LocalContext.current
-
-  BaseReviewScreen(
-      title = AddReviewScreenStrings.Title,
-      huntTitle = uiState.hunt?.title ?: "",
-      authorName = author,
-      rating = uiState.rating,
-      reviewText = uiState.reviewText,
-      photos = uiState.photos,
-      isReviewTextError = uiState.invalidReviewText != null,
-      isDoneEnabled = uiState.isValid,
-      onRatingChanged = { reviewViewModel.updateRating(it.toDouble()) },
-      onReviewTextChanged = { reviewViewModel.setReviewText(it) },
-      onAddPhotos = { imagePickerLauncher.launch(AddReviewScreenStrings.ImageMimeType) },
-      onRemovePhoto = { index -> reviewViewModel.removePhoto(uiState.photos[index]) },
-      onGoBack = onGoBack,
-      onCancel = {
-        reviewViewModel.clearFormNoSubmission()
-        onCancel()
-      },
-      onDone = {
-        hunt?.let { reviewViewModel.submitCurrentUserReview(it, context) }
-        onDone()
-      })
-
-     */
 }
 
 @Composable
