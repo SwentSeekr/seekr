216094c222163de4225a8924641921682a1e8898
Matax606 <supermaxyoohoo@gmail.com>
2025-12-17 16:09:03 +0100

    fix: solve merging conflicts

--- app/src/main/java/com/swentseekr/seekr/ui/settings/SettingsScreenConstants.kt
+++ app/src/main/java/com/swentseekr/seekr/ui/settings/SettingsScreenConstants.kt
@@ -50,8 +50,6 @@ object SettingsScreenStrings {
 
 object SettingsScreenTestTags {
   const val SETTINGS_SCREEN = "settingsScreen"
-  const val LOGOUT_BUTTON = "logoutButton"
-  const val APP_VERSION_TEXT = "appVersionText"
   const val BACK_BUTTON = "backButton"
   const val EDIT_PROFILE_BUTTON = "editProfileButton"
 
